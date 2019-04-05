ca090c5df56aeb243f4bfc170cc0be045f678038
anousone <touya_ra@hotmail.com>
2019-04-05 12:23:28 +0700

    on login

--- src/controllers/UserAccessController.ts
+++ src/controllers/UserAccessController.ts
@@ -5,35 +5,49 @@ import * as utility from '../scripts/utility';
 import { Request, Response } from 'express';
 import { NextFunction } from 'connect';
 
+var __design_view = "objectList";
 
 export class userAccessController {
-
+    
     public authentication(req: Request, res: Response) {
         let uti=new utility.utility();
         let js =req.body;
         (!js.data || js.data === undefined) ? js.data = {} : js.data;
         let usr = js.data.user;
-        let db=uti.createDB('taskmanageruser');
-        db.then((res)=>{
-            res.view();
-            res.send(loginModel);
+        let db=uti.create_db('taskmanageruser');
+        // CHECK ACCESS TOKEN & IP 
+        // AUTHENTICATION
+        db.then((re)=>{
+            re.view('_designObject', 'authentication', {
+                'keys': ['username', 'password']
+              }).then((body) => {
+                if(body.rows.length){
+                    js.client.logintoken="";
+                    js.client.logintime=new Date();
+                    js.client.data.user={};
+                    js.client.message="OK LOGIN";
+                    res.send(js);
+                }else{
+                    js.client.logintoken="";
+                    js.client.logintime="";
+                    js.client.data.user={};
+                    js.client.message="ERROR WRONG USERNAME OR PASSWORD";
+                    res.send(js);
+                }
+              });
         }).catch((err)=>{
             res.send(js)
         });
-        res.send(loginModel);
     }
     public accessControl(req: Request, res: Response,next: NextFunction) {
         let uti=new utility.utility();
         let js =req.body;
         (!js.data || js.data === undefined) ? js.data = {} : js.data;
         let usr = js.data.user;
-        let db=uti.createDB('taskmanageruser');
-        db.then((res)=>{
-            res.view();
-            res.send(loginModel);
-        }).catch((err)=>{
-            res.send(js)
-        });
-        res.send(loginModel);
+        // CHECK ACCESS TOKEN & IP 
+        // CHECK LOGIN TOKEN ( AUTHORIZATION )
+        // PASS IF LOGIN OK
+        // GO TO LOGIN IF LOGIN ERROR
+        
     }
 }
--- src/scripts/utility.ts
+++ src/scripts/utility.ts
@@ -1,6 +1,13 @@
 import * as uuidv from 'uuid';
 import * as Nano from "nano";
+import * as async from 'async';
+import * as Q from 'q';
+import * as redis from 'redis';
+import * as loginModel from '../models/LoginModel';
+
 let nano = Nano('http://localhost:5984');
+let r_client =redis.createClient();
+let _current_system="tastmanagement";
 export class utility {
     public isJSON(str: any) {
         try {
@@ -13,9 +20,159 @@ export class utility {
     public genUUID(){
         return uuidv();
     }
-    public async createDB(dbname:string):Promise<any>{
-        //await nano.db.destroy(dbname)
-        await nano.db.create(dbname);
+    public async create_db(dbname:string):Promise<Nano.DocumentScope<any>>{
+        nano.db.create(dbname, (err, body) => {
+            // specify the database we are going to use    
+            if (!err) {
+                console.log('database ' + dbname + ' created!');
+            } else
+                console.log(dbname + " could not be created!");
+        });
         return nano.use(dbname);
       }
+    public async initDB(dbname:string,design:any):Promise<any>{
+        // create a new database
+        var db:any;
+        async.eachSeries([
+            db = this.create_db(dbname),
+            db = nano.use(dbname),
+            db.insert(design, (err, res) => {
+                if (err) {
+                    //console.log(design);
+                    //console.log(err);
+                    db.get('_design/objectList', (err, res) => {
+                        if (err) {
+                            console.log(dbname);
+                            console.log(design);
+                            console.log('could not find design ' + err);
+                        } else {
+                            if (res) {
+                                var d = res;
+                                //console.log("d:"+JSON.stringify(d));
+                                db.destroy('_design/objectList', d._rev, (err, res) => {
+                                    if (err) console.log(err);
+                                    else {
+                                        //console.log(res);
+                                        db.insert(design, "_design/objectList", (err, res) => {
+                                            if (err) console.log('err insert new design ' + dbname + err);
+                                            else {
+                                                //console.log('insert design completed ' + dbname);
+                                            }
+                                        });
+                                    }
+                                });
+                            } else {
+                                db.insert(design, "_design/objectList", (err, res) => {
+                                    if (err) console.log('err insert new design ' + dbname + err);
+                                    else {
+                                        //console.log('insert design completed ' + dbname);
+                                    }
+                                });
+                                // console.log("could not find design");
+                            }
+                        }
+                    });
+                } else {
+                    console.log('created design ' + dbname);
+                }
+
+            })
+        ], (err) => {
+            console.log('exist ' + dbname);
+        });
+    }
+    checkAuthorize(js) {
+        let deferred = Q.defer();
+        //deferred.resolve(js); // JUST BY PASS THIS TEMPORARY
+        //if (0)
+        try {
+            let except = ['ping', 'login', 'shake-hands', 'heart-beat', 'register',
+                'check-secret', 'get-secret', 'submit-forgot', 'check-forgot', 'reset-forgot',
+                'check-username', 'check-password', 'check-phone'
+            ];
+            if (except.indexOf(js.client.data.command) > -1) {
+                js.client.data.message = 'OK';
+                deferred.resolve(js);
+            } else {
+                r_client.get(_current_system + '_login_' + js.client.logintoken, (err, r) => {
+                    if (err) {
+                        js.client.data.message = err;
+                        deferred.reject(js);
+                    } else {
+                        if (r) {
+                            let res = JSON.parse(r) as loginObj;
+                            if (res.client.logintoken !== undefined) {
+                                js.client.data.message = 'OK';
+                                r_client.get(_current_system + '_usergui_' + js.client.logintoken, (err, r) => {
+                                    if (err) {
+                                        js.client.data.message = err;
+                                        deferred.reject(js);
+                                    } else {
+                                        let res = JSON.parse(r) as guiObj;
+                                        if (res.gui) {
+                                            js.client.data.message = 'OK authorized'
+                                            js.client.auth = {};
+                                            js.client.auth.gui = res.gui;
+                                            deferred.resolve(js);
+                                        } else {
+                                            js.client.data.message = new Error('ERROR empty login');
+                                            deferred.resolve(js);
+                                        }
+                                    }
+                                });
+                            } else {
+                                js.client.data.message = new Error('ERROR empty login');
+                                deferred.resolve(js);
+                            }
+                        }
+                        else {
+                            js.client.data.message = new Error('ERROR empty login');
+                            deferred.resolve(js);
+                        }
+                    }
+                });
+            }
+
+        } catch (error) {
+            js.client.data.message = error;
+            deferred.reject(js);
+        }
+        return deferred.promise;
+    }
+}
+export interface guiObj {
+    command: string;
+    gui: string;
+}
+export interface loginObj {
+    command: string;
+    client: any;
+}
+export interface client {
+    gui: '',
+    username: '',
+    logintoken: '',
+    logintime: '',
+    loginip: '',
+    data: any,
+    auth: any
+};
+export interface phoneObj {
+    command: string;
+    secret: string;
+}
+export interface onlineObj {
+    command: string;
+    client: {
+        username: string;
+        onlinetime: Date;
+        system: string;
+        login: Array<any>;
+    }
+}
+export interface photoObj {
+    name: string;
+    arraybuffer: string;
+    type: string;
+    url: string;
 }
